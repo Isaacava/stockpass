3d118dfa4ba89e4ce36e0ee542049dd17ba28b99
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-09-15 09:52:39 +0100

    Mount StockPass market utility in connected workspace

---
+++ src/StockPassWorkspace.tsx
@@ -0,0 +1,11 @@
+import StockPassApp from './StockPassApp';
+import StockPassUtilityHub from './StockPassUtilityHub';
+
+export default function StockPassWorkspace() {
+  return (
+    <>
+      <StockPassApp />
+      <StockPassUtilityHub />
+    </>
+  );
+}
